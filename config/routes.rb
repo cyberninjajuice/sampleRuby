3e4b774ee5dfb8ca898977d8df5c6ec8804c5cc3
Raz <razgoldin@gmail.com>
2015-05-25 19:00:46 -0400

    adding model user

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,6 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-
-  get 'static_pages/about'
-
-  get 'static_pages/contact'
-
-  get 'static_pages/help'
-
-  root "application#home"
-  get "/contact" => "application#contact"
-  get "/about" => "application#about"
-  get "/help" => "application#help"
+  root "static_pages#home"
+  get "/contact" => "static_pages#contact"
+  get "/about" => "static_pages#about"
+  get "/help" => "static_pages#help"
 end
